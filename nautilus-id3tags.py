bf5d555d46dbed36d52e8c2f67be5750c229be18
Jason Pleau <jason@jpleau.ca>
2014-07-25 14:49:01 -0400

    Add license / rename extension

--- nautilus-id3tags.py
+++ nautilus-id3tags.py
@@ -1,3 +1,21 @@
+# nautilus-id3tags: Nautilus extension to allow for editing of ID3 tags for 
+# audio files.
+# Copyright (C) 2014 Jason Pleau <jason@jpleau.ca>
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 import urllib
 import mimetypes
 
@@ -80,7 +98,7 @@ class ColumnExtension(GObject.GObject, Nautilus.PropertyPageProvider):
         self.__load_genres()
         self.__load_data()
         
-        return Nautilus.PropertyPage(name="NautilusPython::Audio_tag",
+        return Nautilus.PropertyPage(name="NautilusPython::ID3_TAGS",
                                      label=self.property_label, 
                                      page=self.box),
     def __save_tags(self, widget):
