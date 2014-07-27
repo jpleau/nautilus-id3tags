8151e21b4b9f08406ae00346170b58be7e8a1b8f
Jason Pleau <jason@jpleau.ca>
2014-07-27 15:48:34 -0400

    prepare code to add columns in listing

--- nautilus-id3tags.py
+++ nautilus-id3tags.py
@@ -26,22 +26,65 @@ from gi.repository import Nautilus, GObject, Gtk
 
 import taglib
 
-class Tag:
-    
-    def __init__(self, tag_name, read_method, save_method, initial_value):
-        self.tag_name = tag_name
-        self.read_method = read_method
-        self.save_method = save_method
-        self.save_method(initial_value)
+class AudioFile:
+    def __init__(self, filename):
+        self.filename = filename
+
+        self.tags = []
+
+        self.genres = [
+            "Blues", "Classic Rock", "Country", "Dance", "Disco", "Funk", "Grunge", "Hip-Hop", "Jazz", "Metal", 
+            "New Age", "Oldies", "Other", "Pop", "Rhythm and Blues", "Rap", "Reggae", "Rock", "Techno", 
+            "Industrial", "Alternative", "Ska", "Death Metal", "Pranks", "Soundtrack", "Euro-Techno", "Ambient", 
+            "Trip-Hop", "Vocal", "Jazz & Funk", "Fusion", "Trance", "Classical", "Instrumental", "Acid",
+            "House", "Game", "Sound Clip", "Gospel", "Noise", "Alternative Rock", "Bass", "Soul", "Punk", 
+            "Space", "Meditative", "Instrumental Pop", "Instrumental Rock", "Ethnic", "Gothic", "Darkwave", 
+            "Techno-Industrial", "Electronic", "Pop-Folk", "Eurodance", "Dream", "Southern Rock", "Comedy", 
+            "Cult", "Gangsta", "Top 40", "Christian Rap", "Pop/Funk", "Jungle", "Native American", "Cabaret", 
+            "New Wave", "Psychedelic", "Rave", "Showtunes", "Trailer", "Lo-Fi", "Tribal", "Acid Punk", 
+            "Acid Jazz", "Polka", "Retro", "Musical", "Rock & Roll", "Hard Rock", "Folk", "Folk-Rock", 
+            "National Folk", "Swing", "Fast Fusion", "Bebop", "Latin", "Revival", "Celtic", "Bluegrass",
+            "Avantgarde", "Gothic Rock", "Progressive Rock", "Psychedelic Rock", "Symphonic Rock", "Slow Rock",
+            "Big Band", "Chorus", "Easy Listening", "Acoustic", "Humour", "Speech", "Chanson", "Opera", 
+            "Chamber Music", "Sonata", "Symphony", "Booty Bass", "Primus", "Porn groove", "Satire", 
+            "Slow Jam", "Club", "Tango", "Samba", "Folklore", "Ballad", "Power Ballad", "Rhythmic Soul", 
+            "Freestyle", "Duet", "Punk Rock", "Drum Solo", "A capella", "Euro-House", "Dance Hall", "Goa", 
+            "Drum & Bass", "Club-House", "Hardcore Techno", "Terror", "Indie", "BritPop", "Afro-punk", 
+            "Polsk Punk", "Beat", "Christian Gangsta Rap", "Heavy Metal", "Black Metal", "Crossover", 
+            "Contemporary Christian", "Christian Rock", "Merengue", "Salsa", "Thrash Metal", "Anime", "Jpop",
+            "Synthpop", "Abstract", "Art Rock", "Baroque", "Bhangra", "Big Beat", "Breakbeat", "Chillout",
+            "Downtempo", "Dub", "EBM", "Eclectic", "Electro", "Electroclash", "Emo", "Experimental", "Garage", 
+            "Global", "IDM", "Illbient", "Industro-Goth", "Jam Band", "Krautrock", "Leftfield", "Lounge", 
+            "Math Rock", "New Romantic", "Nu-Breakz", "Post-Punk", "Post-Rock", "Psytrance", "Shoegaze", 
+            "Space Rock", "Trop Rock", "World Music", "Neoclassical", "Audiobook", "Audio Theatre", 
+            "Neue Deutsche Welle", "Podcast", "Indie Rock", "G-Funk", "Dubstep", "Garage Rock", "Psybient"
+        ]
+
+        try:
+            self.opened_file = taglib.File(filename)
+        except:
+            raise Error("Error loading file {0}".format(filename))
 
     def save(self):
-        save_method(read_method)
+        for tag in self.tags:
+            value = tag["get_method"]()
+            if value == None:
+                value = ""
+            self.opened_file.tags[tag["tag_name"]] = [value.encode()]
 
+        self.opened_file.save()
 
+    def add_tag(self, tag_name, get_method, set_method):
+        tag_name = tag_name.upper()
+        self.tags.append({
+            "tag_name": tag_name,
+            "get_method": get_method,
+            "set_method": set_method,
+        })
 
+        set_method(self.opened_file.tags[tag_name.upper()][0].decode())
+       
 class NautilusID3Tags(GObject.GObject, Nautilus.PropertyPageProvider):
-
-    genres =  ["Blues", "Classic Rock", "Country", "Dance", "Disco", "Funk", "Grunge", "Hip-Hop", "Jazz", "Metal", "New Age", "Oldies", "Other", "Pop", "Rhythm and Blues", "Rap", "Reggae", "Rock", "Techno", "Industrial", "Alternative", "Ska", "Death Metal", "Pranks", "Soundtrack", "Euro-Techno", "Ambient", "Trip-Hop", "Vocal", "Jazz & Funk", "Fusion", "Trance", "Classical", "Instrumental", "Acid", "House", "Game", "Sound Clip", "Gospel", "Noise", "Alternative Rock", "Bass", "Soul", "Punk", "Space", "Meditative", "Instrumental Pop", "Instrumental Rock", "Ethnic", "Gothic", "Darkwave", "Techno-Industrial", "Electronic", "Pop-Folk", "Eurodance", "Dream", "Southern Rock", "Comedy", "Cult", "Gangsta", "Top 40", "Christian Rap", "Pop/Funk", "Jungle", "Native American", "Cabaret", "New Wave", "Psychedelic", "Rave", "Showtunes", "Trailer", "Lo-Fi", "Tribal", "Acid Punk", "Acid Jazz", "Polka", "Retro", "Musical", "Rock & Roll", "Hard Rock", "Folk", "Folk-Rock", "National Folk", "Swing", "Fast Fusion", "Bebop", "Latin", "Revival", "Celtic", "Bluegrass", "Avantgarde", "Gothic Rock", "Progressive Rock", "Psychedelic Rock", "Symphonic Rock", "Slow Rock", "Big Band", "Chorus", "Easy Listening", "Acoustic", "Humour", "Speech", "Chanson", "Opera", "Chamber Music", "Sonata", "Symphony", "Booty Bass", "Primus", "Porn groove", "Satire", "Slow Jam", "Club", "Tango", "Samba", "Folklore", "Ballad", "Power Ballad", "Rhythmic Soul", "Freestyle", "Duet", "Punk Rock", "Drum Solo", "A capella", "Euro-House", "Dance Hall", "Goa", "Drum & Bass", "Club-House", "Hardcore Techno", "Terror", "Indie", "BritPop", "Afro-punk", "Polsk Punk", "Beat", "Christian Gangsta Rap", "Heavy Metal", "Black Metal", "Crossover", "Contemporary Christian", "Christian Rock", "Merengue", "Salsa", "Thrash Metal", "Anime", "Jpop", "Synthpop", "Abstract", "Art Rock", "Baroque", "Bhangra", "Big Beat", "Breakbeat", "Chillout", "Downtempo", "Dub", "EBM", "Eclectic", "Electro", "Electroclash", "Emo", "Experimental", "Garage", "Global", "IDM", "Illbient", "Industro-Goth", "Jam Band", "Krautrock", "Leftfield", "Lounge", "Math Rock", "New Romantic", "Nu-Breakz", "Post-Punk", "Post-Rock", "Psytrance", "Shoegaze", "Space Rock", "Trop Rock", "World Music", "Neoclassical", "Audiobook", "Audio Theatre", "Neue Deutsche Welle", "Podcast", "Indie Rock", "G-Funk", "Dubstep", "Garage Rock", "Psybient"]
 
     def __init__(self):
         pass
@@ -59,12 +102,10 @@ class NautilusID3Tags(GObject.GObject, Nautilus.PropertyPageProvider):
         # TODO: manage exceptions in a non-retarded way like this :)
 
         try:
-            self.audio_file = taglib.File(filename)
+            self.audio_file = AudioFile(filename)
         except:
             return
         
-        self.tags = []
-
         self.box = Gtk.Box()
         self.box.set_border_width(12)
 
@@ -130,8 +171,8 @@ class NautilusID3Tags(GObject.GObject, Nautilus.PropertyPageProvider):
         self.box.show_all()
         self.saved_label.hide()
 
-        for idx, genre in enumerate(self.__class__.genres):
-            self.genre_combo.append(str(idx), genre)
+        for idx, genre in enumerate(self.audio_file.genres):
+            self.genre_combo.append(genre, genre)
 
         self.load_data()
 
@@ -139,37 +180,30 @@ class NautilusID3Tags(GObject.GObject, Nautilus.PropertyPageProvider):
                                      label=self.property_label, 
                                      page=self.box),
     def save_tags(self, widget):
-        for tag in tags:
-            tag.save()
-
         self.audio_file.save()
         self.saved_label.show()
 
-    def save_tag(self, tag_name, tag_value):
-        self.song_tags[tag_name] = tag_value
-    
-
     def load_data(self):
-        print(self.audio_file.tags)
-        self.length_value.set_text(self.convert_s_to_human(self.audio_file.length))
-        self.tags.append(Tag("title", self.title_entry.get_text, self.title_entry.set_text, self.audio_file.tags["TITLE"][0].decode()))
-        self.tags.append(Tag("artist", self.artist_entry.get_text, self.artist_entry.set_text, self.audio_file.tags["ARTIST"][0].decode()))
-        self.tags.append(Tag("album", self.album_entry.get_text, self.album_entry.set_text, self.audio_file.tags["ALBUM"][0].decode()))
-        self.tags.append(Tag("genre", self.genre_combo.get_active_text, self.genre_combo.set_active, self.__class__.genres.index(self.audio_file.tags["GENRE"][0])))
-        self.tags.append(Tag("date", self.date_entry.get_text, self.date_entry.set_text, self.audio_file.tags["DATE"][0].decode()))
-
-    def convert_s_to_human(self, seconds):
-        x = seconds / 60
-        seconds = seconds % 60
-
-        minutes = x % 60
-        hours = x / 60
-
-        value = "{0:02d}:{1:02d}".format(minutes, seconds)
-
-        if hours > 0:
-            value = "{0:02d}".format(hours) + value
-
-        return value
+        self.length_value.set_text(convert_s_to_human(self.audio_file.opened_file.length))
+        self.audio_file.add_tag("title", self.title_entry.get_text, self.title_entry.set_text)
+        self.audio_file.add_tag("title", self.title_entry.get_text, self.title_entry.set_text)
+        self.audio_file.add_tag("artist", self.artist_entry.get_text, self.artist_entry.set_text)
+        self.audio_file.add_tag("album", self.album_entry.get_text, self.album_entry.set_text)
+        self.audio_file.add_tag("genre", self.genre_combo.get_active_text, self.genre_combo.set_active_id)
+        self.audio_file.add_tag("date", self.date_entry.get_text, self.date_entry.set_text)
 
 
+
+def convert_s_to_human(seconds):
+    x = seconds / 60
+    seconds = seconds % 60
+
+    minutes = x % 60
+    hours = x / 60
+
+    value = "{0:02d}:{1:02d}".format(minutes, seconds)
+
+    if hours > 0:
+        value = "{0:02d}".format(hours) + value
+
+    return value
