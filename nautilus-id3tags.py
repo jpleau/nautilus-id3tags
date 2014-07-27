58fe62c3e71a910ffeef1b5400ac8e5d91265a63
Jason Pleau <jason@jpleau.ca>
2014-07-27 01:22:31 -0400

    switch to taglib

--- nautilus-id3tags.py
+++ nautilus-id3tags.py
@@ -23,10 +23,26 @@ import mimetypes
 import datetime
 
 from gi.repository import Nautilus, GObject, Gtk
-from mutagen.easyid3 import EasyID3
-from mutagen._constants import GENRES
+
+import taglib
+
+class Tag:
+    
+    def __init__(self, tag_name, read_method, save_method, initial_value):
+        self.tag_name = tag_name
+        self.read_method = read_method
+        self.save_method = save_method
+        self.save_method(initial_value)
+
+    def save(self):
+        save_method(read_method)
+
+
 
 class NautilusID3Tags(GObject.GObject, Nautilus.PropertyPageProvider):
+
+    genres =  ["Blues", "Classic Rock", "Country", "Dance", "Disco", "Funk", "Grunge", "Hip-Hop", "Jazz", "Metal", "New Age", "Oldies", "Other", "Pop", "Rhythm and Blues", "Rap", "Reggae", "Rock", "Techno", "Industrial", "Alternative", "Ska", "Death Metal", "Pranks", "Soundtrack", "Euro-Techno", "Ambient", "Trip-Hop", "Vocal", "Jazz & Funk", "Fusion", "Trance", "Classical", "Instrumental", "Acid", "House", "Game", "Sound Clip", "Gospel", "Noise", "Alternative Rock", "Bass", "Soul", "Punk", "Space", "Meditative", "Instrumental Pop", "Instrumental Rock", "Ethnic", "Gothic", "Darkwave", "Techno-Industrial", "Electronic", "Pop-Folk", "Eurodance", "Dream", "Southern Rock", "Comedy", "Cult", "Gangsta", "Top 40", "Christian Rap", "Pop/Funk", "Jungle", "Native American", "Cabaret", "New Wave", "Psychedelic", "Rave", "Showtunes", "Trailer", "Lo-Fi", "Tribal", "Acid Punk", "Acid Jazz", "Polka", "Retro", "Musical", "Rock & Roll", "Hard Rock", "Folk", "Folk-Rock", "National Folk", "Swing", "Fast Fusion", "Bebop", "Latin", "Revival", "Celtic", "Bluegrass", "Avantgarde", "Gothic Rock", "Progressive Rock", "Psychedelic Rock", "Symphonic Rock", "Slow Rock", "Big Band", "Chorus", "Easy Listening", "Acoustic", "Humour", "Speech", "Chanson", "Opera", "Chamber Music", "Sonata", "Symphony", "Booty Bass", "Primus", "Porn groove", "Satire", "Slow Jam", "Club", "Tango", "Samba", "Folklore", "Ballad", "Power Ballad", "Rhythmic Soul", "Freestyle", "Duet", "Punk Rock", "Drum Solo", "A capella", "Euro-House", "Dance Hall", "Goa", "Drum & Bass", "Club-House", "Hardcore Techno", "Terror", "Indie", "BritPop", "Afro-punk", "Polsk Punk", "Beat", "Christian Gangsta Rap", "Heavy Metal", "Black Metal", "Crossover", "Contemporary Christian", "Christian Rock", "Merengue", "Salsa", "Thrash Metal", "Anime", "Jpop", "Synthpop", "Abstract", "Art Rock", "Baroque", "Bhangra", "Big Beat", "Breakbeat", "Chillout", "Downtempo", "Dub", "EBM", "Eclectic", "Electro", "Electroclash", "Emo", "Experimental", "Garage", "Global", "IDM", "Illbient", "Industro-Goth", "Jam Band", "Krautrock", "Leftfield", "Lounge", "Math Rock", "New Romantic", "Nu-Breakz", "Post-Punk", "Post-Rock", "Psytrance", "Shoegaze", "Space Rock", "Trop Rock", "World Music", "Neoclassical", "Audiobook", "Audio Theatre", "Neue Deutsche Welle", "Podcast", "Indie Rock", "G-Funk", "Dubstep", "Garage Rock", "Psybient"]
+
     def __init__(self):
         pass
     
@@ -40,15 +56,13 @@ class NautilusID3Tags(GObject.GObject, Nautilus.PropertyPageProvider):
 
         filename = urllib.unquote(file.get_uri()[7:])
     
-        # For now I am using EasyID3, I will swap to ID3 eventually and won't have to 
-        # do this ugly workaround to not process non-audio files (files without id3 tags..)
+        # TODO: manage exceptions in a non-retarded way like this :)
+
         try:
-            self.song_tags = EasyID3(filename)
+            self.audio_file = taglib.File(filename)
         except:
             return
-
-        self.genres = {}
-
+        
         self.box = Gtk.Box()
         self.box.set_border_width(12)
 
@@ -74,8 +88,8 @@ class NautilusID3Tags(GObject.GObject, Nautilus.PropertyPageProvider):
         self.genre_label = Gtk.Label("Genre: ", xalign=0)
         self.genre_combo = Gtk.ComboBoxText()
 
-        self.year_label = Gtk.Label("Year: ", xalign=0)
-        self.year_entry = Gtk.Entry()
+        self.date_label = Gtk.Label("Year: ", xalign=0)
+        self.date_entry = Gtk.Entry()
 
         self.length_label = Gtk.Label("Length: ", xalign=0)
         self.length_value = Gtk.Label("", xalign=0)
@@ -94,8 +108,8 @@ class NautilusID3Tags(GObject.GObject, Nautilus.PropertyPageProvider):
         self.grid.attach(self.genre_label, 1, 3, 1, 1)
         self.grid.attach(self.genre_combo, 2, 3, 1, 1)
 
-        self.grid.attach(self.year_label, 1, 4, 1, 1)
-        self.grid.attach(self.year_entry, 2, 4, 1, 1)
+        self.grid.attach(self.date_label, 1, 4, 1, 1)
+        self.grid.attach(self.date_entry, 2, 4, 1, 1)
 
         self.grid.attach(self.length_label, 1, 5, 1, 1)
         self.grid.attach(self.length_value, 2, 5, 1, 1)
@@ -106,7 +120,7 @@ class NautilusID3Tags(GObject.GObject, Nautilus.PropertyPageProvider):
 
         self.button = Gtk.Button("Save")
         self.grid.attach(self.button, 1, nb_items+1, 1, 1)
-        self.button.connect("clicked", self.__save_tags)
+        self.button.connect("clicked", self.save_tags)
 
         self.saved_label = Gtk.Label("Changes saved.", xalign=0)
         self.grid.attach(self.saved_label, 2, nb_items+1, 1, 1)
@@ -114,52 +128,38 @@ class NautilusID3Tags(GObject.GObject, Nautilus.PropertyPageProvider):
         self.box.show_all()
         self.saved_label.hide()
 
-        self.__load_genres()
-        self.__load_data()
-        
+        for idx, genre in enumerate(self.__class__.genres):
+            self.genre_combo.append(str(idx), genre)
+
+        self.load_data()
+
         return Nautilus.PropertyPage(name="NautilusPython::ID3_TAGS",
                                      label=self.property_label, 
                                      page=self.box),
-    def __save_tags(self, widget):
-        self.song_tags["title"] = self.title_entry.get_text()
-        self.song_tags["artist"] = self.artist_entry.get_text()
-        self.song_tags["album"] = self.album_entry.get_text()
-        self.song_tags["genre"] = [self.genre_combo.get_active_text()]
-        self.song_tags["date"] = self.year_entry.get_text()
-        self.song_tags.save()
+    def save_tags(self, widget):
+        for tag in tags:
+            tag.save()
+
+        self.audio_file.save()
         self.saved_label.show()
 
-
-    def __load_genres(self):
-        for i in range(len(GENRES)):
-            self.genre_combo.insert(i, str(i), GENRES[i])
-            self.genres[GENRES[i]] = i
-
+    def save_tag(self, tag_name, tag_value):
+        self.song_tags[tag_name] = tag_value
     
-    def __load_data(self):
-        if "title" in self.song_tags:
-            self.title_entry.set_text(self.song_tags["title"][0])
-        if "artist" in self.song_tags:
-            self.artist_entry.set_text(self.song_tags["artist"][0])
 
-        if "album" in self.song_tags:
-            self.album_entry.set_text(self.song_tags["album"][0])
+    def load_data(self):
+        print(self.audio_file.tags)
+        self.length_value.set_text(self.convert_s_to_human(self.audio_file.length))
+        self.tags.append(Tag("title", self.title_entry.get_text, self.title_entry.set_text, self.audio_file.tags["TITLE"][0].decode()))
+        self.tags.append(Tag("artist", self.artist_entry.get_text, self.artist_entry.set_text, self.audio_file.tags["ARTIST"][0].decode()))
+        self.tags.append(Tag("album", self.album_entry.get_text, self.album_entry.set_text, self.audio_file.tags["ALBUM"][0].decode()))
+        self.tags.append(Tag("genre", self.genre_combo.get_active_text, self.genre_combo.set_active, self.__class__.genres.index(self.audio_file.tags["GENRE"][0])))
+        self.tags.append(Tag("date", self.date_entry.get_text, self.date_entry.set_text, self.audio_file.tags["DATE"][0].decode()))
 
-        if "genre" in self.song_tags:
-            self.genre_combo.set_active(self.genres[self.song_tags["genre"][0]])
+    def convert_s_to_human(self, seconds):
+        x = seconds / 60
+        seconds = seconds % 60
 
-        if "date" in self.song_tags:
-            self.year_entry.set_text(self.song_tags["date"][0])
-
-        if "length" in self.song_tags and len(self.song_tags["length"]) > 0:
-            self.length_value.set_text(self.__convert_ms_to_human(int(self.song_tags["length"][0])))
-        else:
-            self.length_value.set_text("N/A")
-
-    def __convert_ms_to_human(self, ms):
-        x = ms / 1000
-        seconds = x % 60
-        x /= 60
         minutes = x % 60
         hours = x / 60
 
@@ -169,4 +169,5 @@ class NautilusID3Tags(GObject.GObject, Nautilus.PropertyPageProvider):
             value = "{0:02d}".format(hours) + value
 
         return value
+
 
